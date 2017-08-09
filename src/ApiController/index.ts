155abe88d8ca61e2b84866ffebf6ff98c5bd8aef
Torstein Bjørnstad <tosh@tosh.no>
2017-08-09 20:58:27 +0200

    Remove events

--- src/ApiController/index.ts
+++ src/ApiController/index.ts
@@ -1,7 +1,6 @@
 import Deps from "../Deps";
 import {Router, Response} from "express";
 import * as cors from 'cors';
-import EventsController from './EventsController';
 import CreditsController from './CreditsController';
 import PlayersController from './PlayersController';
 
@@ -12,7 +11,6 @@ export default class ApiRouter {
     this.router = Router();
     this.router.use(cors());
 
-    this.router.use('/events', new EventsController(deps).router);
     this.router.use('/credits', new CreditsController(deps).router);
     this.router.use('/players', new PlayersController(deps).router);
     this.router.get('/', (_req: any, res: Response) => {
--- src/Deps/index.ts
+++ src/Deps/index.ts
@@ -1,10 +1,8 @@
 import CreditsRepository from "./CreditsRepository";
 import PlayerRepository from "./PlayerRepository";
 import Config from "../Config";
-import FacebookEventLoader from "./FacebookEventLoader";
 
 export default class Deps {
-  public facebookEventLoader: FacebookEventLoader;
   public creditsRepository: CreditsRepository;
   public playerRepository: PlayerRepository;
   public config: Config;
@@ -13,6 +11,5 @@ export default class Deps {
     this.config = config;
     this.creditsRepository = new CreditsRepository();
     this.playerRepository = new PlayerRepository(config);
-    this.facebookEventLoader = new FacebookEventLoader(config);
   }
 }
